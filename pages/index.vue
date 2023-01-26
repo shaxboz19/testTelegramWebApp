3480e699cd55c7b2bd1bd82ebb8eec659a33d72b
shaxboz99 <shaxboz.nv37@gmail.com>
2023-01-26 23:16:07 +0500

    some changes

--- pages/index.vue
+++ pages/index.vue
@@ -14,13 +14,8 @@ export default {
   methods: {
     async onSubmit () {
       try {
-        const xml = '<TKKPG><Request><Operation>CreateOrder</Operation><Language>RU</Language><Order><OrderType>Purchase</OrderType><Merchant>SLOMBARD20001748</Merchant><Amount>100</Amount><Currency>398</Currency><Description>Test</Description><AddParams><OrderExpirationPeriod>30</OrderExpirationPeriod></AddParams></Order></Request></TKKPG>'
-        const config = {
-          headers: { 'Content-Type': 'text/xml' }
-        }
         await this.$axios.post(
-          'https://epay.fortebank.com/cardregistrationindex.jsp?approveUrl=https://klinika.itsone.uz/test&declineUrl=https://klinika.itsone.uz/test',
-          xml, config
+          'https://klinika.itsone.uz/request'
         )
       } catch (error) {
         console.log(error)
